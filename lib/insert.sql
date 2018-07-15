c34c2d24335e4f39610b48aaeb83475ddd8eb958
Marcus Jun <mjun@calstate.edu>
2018-07-15 00:58:23 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'docile', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 10, 'F', 'black', 'wild', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 6, 'M', 'gray', 'docile', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 2, 'F', 'black', 'wild', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'brown', 'docile', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 8, 'M', 'black', 'wild', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 1, 'M', 'gray', 'wild', 1);
